b3578d4aa2d82edb74dd262f8342a9936b483243
Dizzy-codes <68827682+Dizzy-codes@users.noreply.github.com>
2022-02-03 23:04:15 -0500

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -11,13 +11,16 @@ import collections
 from collections import Counter
 from table import *
 
+
+
 def evaluate_board():
     #Seprate board use to evalue the position on the main board
     BOARD_REPLICA = chess.Board()
     #Main chess; the board the players use
     BOARD = chess.Board()
-    WHITE = True
+    PLAYER_COLOR_WHITE = True
     first_move = True
+    index = 0
 
     #Value to determine if white made a move
     moved = 0
@@ -29,7 +32,7 @@ def evaluate_board():
         chess.KNIGHT: 320,
         chess.BISHOP: 330,
         chess.QUEEN: 900,
-        chess.KING: 200000000
+        chess.KING: 9999999999
     }
 
     #Stores the index of the move(ie: e2)
@@ -37,6 +40,12 @@ def evaluate_board():
 
     #Stores the value of the square spot from the table
     CHESS_SQUARE_VALUE = {}
+
+    #Holds the original value of the square
+    ORIGINAL_SQUARE_VALUE = {}
+
+    #Holds the current chess piece
+    CURRENT_SQUARE_PIECE = []
 
     #Contains first moves for the bot
     FIRST_MOVES = ["e2e4", "d2d4", "c2c4", "b1c3", "g1f3"]
@@ -47,22 +56,103 @@ def evaluate_board():
 
     #While the game is active (NOT ENDED)
     while BOARD.is_checkmate() == False and BOARD.is_stalemate() == False and BOARD.is_insufficient_material() == False and BOARD.can_claim_draw() == False:
-        #Changes White's value based on the response
+
+
+        #If the player choose White
         if player_color == "W":
-            try:
-                WHITE = True
-                player_move = input("Enter you move (Example: e2e4 would be = to Pe4) ")
-                BOARD.push_san(player_move)
-                BOARD_REPLICA.push_san(player_move)
-            except error as E:
-                   print(f"Error:{E}") 
+            PLAYER_COLOR_WHITE = True
+            player_move = input("Enter you move (Example: e2e4 would be = to Pe4) ")
+            BOARD.push_san(player_move)
+            BOARD_REPLICA.push_san(player_move)
+
+            #Finds the chess piece on the inputed square
+            player_chess_piece = str(BOARD.piece_at(chess.parse_square(player_move[-2:])))
+            player_move_index = chess.parse_square(str(player_move)[-2:])
+
+            #Add the original square value based on the piece just moved. NOTE: before incentivizing sqaures occurs
+            if player_chess_piece == "P":
+                ORIGINAL_SQUARE_VALUE[player_move] = pawntable[player_move_index]
+            elif player_chess_piece == "R":
+                ORIGINAL_SQUARE_VALUE[player_move] = rookstable[player_move_index]
+            elif player_chess_piece == "N":
+                ORIGINAL_SQUARE_VALUE[player_move] = knightstable[player_move_index]
+            elif player_chess_piece == "B":
+                ORIGINAL_SQUARE_VALUE[player_move] = bishopstable[player_move_index]
+            elif player_chess_piece == "Q":
+                ORIGINAL_SQUARE_VALUE[player_move] = queenstable[player_move_index]
+            else:
+                ORIGINAL_SQUARE_VALUE[player_move] = kingstable[player_move_index]
+
+            CURRENT_SQUARE_PIECE.append(player_chess_piece)
+
+            print(ORIGINAL_SQUARE_VALUE)
+
+            #Increases a square's value, based on what piece is there
+            #NOTE: YOU MUST UPDATE ALL TABLES
+            if player_chess_piece == "P":
+                pawntable[player_move_index] = pawntable[player_move_index] + CHESS_PIECE_VALUES[chess.PAWN]
+                rookstable[player_move_index] = rookstable[player_move_index] + CHESS_PIECE_VALUES[chess.PAWN]
+                knightstable[player_move_index] = knightstable[player_move_index] + CHESS_PIECE_VALUES[chess.PAWN]
+                bishopstable[player_move_index] = bishopstable[player_move_index] + CHESS_PIECE_VALUES[chess.PAWN]
+                queenstable[player_move_index] = queenstable[player_move_index] + CHESS_PIECE_VALUES[chess.PAWN]
+                kingstable[player_move_index] = kingstable[player_move_index] + CHESS_PIECE_VALUES[chess.PAWN]
+            elif player_chess_piece == "R":
+                pawntable[player_move_index] = pawntable[player_move_index] + CHESS_PIECE_VALUES[chess.ROOK]
+                rookstable[player_move_index] = rookstable[player_move_index] + CHESS_PIECE_VALUES[chess.ROOK]
+                knightstable[player_move_index] = knightstable[player_move_index] + CHESS_PIECE_VALUES[chess.ROOK]
+                bishopstable[player_move_index] = bishopstable[player_move_index] + CHESS_PIECE_VALUES[chess.ROOK]
+                queenstable[player_move_index] = queenstable[player_move_index] + CHESS_PIECE_VALUES[chess.ROOK]
+                kingstable[player_move_index] = kingstable[player_move_index] + CHESS_PIECE_VALUES[chess.ROOK]
+            elif player_chess_piece == "N":
+                pawntable[player_move_index] = pawntable[player_move_index] + CHESS_PIECE_VALUES[chess.KNIGHT]
+                rookstable[player_move_index] = rookstable[player_move_index] + CHESS_PIECE_VALUES[chess.KNIGHT]
+                knightstable[player_move_index] = knightstable[player_move_index] + CHESS_PIECE_VALUES[chess.KNIGHT]
+                bishopstable[player_move_index] = bishopstable[player_move_index] + CHESS_PIECE_VALUES[chess.KNIGHT]
+                queenstable[player_move_index] = queenstable[player_move_index] + CHESS_PIECE_VALUES[chess.KNIGHT]
+                kingstable[player_move_index] = kingstable[player_move_index] + CHESS_PIECE_VALUES[chess.KNIGHT]
+            elif player_chess_piece == "B":
+                pawntable[player_move_index] = pawntable[player_move_index] + CHESS_PIECE_VALUES[chess.BISHOP]
+                rookstable[player_move_index] = rookstable[player_move_index] + CHESS_PIECE_VALUES[chess.BISHOP]
+                knightstable[player_move_index] = knightstable[player_move_index] + CHESS_PIECE_VALUES[chess.BISHOP]
+                bishopstable[player_move_index] = bishopstable[player_move_index] + CHESS_PIECE_VALUES[chess.BISHOP]
+                queenstable[player_move_index] = queenstable[player_move_index] + CHESS_PIECE_VALUES[chess.BISHOP]
+                kingstable[player_move_index] = kingstable[player_move_index] + CHESS_PIECE_VALUES[chess.BISHOP]
+            elif player_chess_piece == "Q":
+                pawntable[player_move_index] = pawntable[player_move_index] + CHESS_PIECE_VALUES[chess.QUEEN]
+                rookstable[player_move_index] = rookstable[player_move_index] + CHESS_PIECE_VALUES[chess.QUEEN]
+                knightstable[player_move_index] = knightstable[player_move_index] + CHESS_PIECE_VALUES[chess.QUEEN]
+                bishopstable[player_move_index] = bishopstable[player_move_index] + CHESS_PIECE_VALUES[chess.QUEEN]
+                queenstable[player_move_index] = queenstable[player_move_index] + CHESS_PIECE_VALUES[chess.QUEEN]
+                kingstable[player_move_index] = kingstable[player_move_index] + CHESS_PIECE_VALUES[chess.QUEEN]
+            else:
+                pawntable[player_move_index] = pawntable[player_move_index] + CHESS_PIECE_VALUES[chess.KING]
+                rookstable[player_move_index] = rookstable[player_move_index] + CHESS_PIECE_VALUES[chess.KING]
+                knightstable[player_move_index] = knightstable[player_move_index] + CHESS_PIECE_VALUES[chess.KING]
+                bishopstable[player_move_index] = bishopstable[player_move_index] + CHESS_PIECE_VALUES[chess.KING]
+                queenstable[player_move_index] = queenstable[player_move_index] + CHESS_PIECE_VALUES[chess.KING]
+                kingstable[player_move_index] = kingstable[player_move_index] + CHESS_PIECE_VALUES[chess.KING]
+
+
+            #Checks if a sqaure value has moved. IF the piece is no longer there, revert the square to its orginal value
+            for key, value in ORIGINAL_SQUARE_VALUE.items():
+                if BOARD.piece_at(chess.parse_square(key[-2:])) == None or str(BOARD.piece_at(chess.parse_square(key[-2:]))) != CURRENT_SQUARE_PIECE[index]:
+                    pawntable[chess.parse_square(str(key[-2:]))] = value
+                    rookstable[chess.parse_square(str(key[-2:]))] = value
+                    knightstable[chess.parse_square(str(key[-2:]))] = value
+                    bishopstable[chess.parse_square(str(key[-2:]))] = value
+                    queenstable[chess.parse_square(str(key[-2:]))] = value
+                    kingstable[chess.parse_square(str(key[-2:]))] = value
+                index += 1
 
         else:
-            WHITE = False
+            PLAYER_COLOR_WHITE = False
 
+
+
+                    
 
         #Makes a random good first move
-        if WHITE == False and first_move == True:
+        if PLAYER_COLOR_WHITE == False and first_move == True:
             random_move = random.choice(FIRST_MOVES)
             BOARD.push_san(random_move)
             BOARD_REPLICA.push_san(random_move)
@@ -71,7 +161,7 @@ def evaluate_board():
             first_move = False
 
         #When the player plays as black
-        if WHITE == False:
+        if PLAYER_COLOR_WHITE == False:
             player_move = input("Enter you move (Example: e2e4 would be = to Pe4) ")
             BOARD.push_san(player_move)
             BOARD_REPLICA.push_san(player_move)
@@ -79,11 +169,91 @@ def evaluate_board():
             print(BOARD)
             print(" ")
 
+            #Finds the chess piece on the inputed square
+            player_chess_piece = str(BOARD.piece_at(chess.parse_square(player_move[-2:])))
+            player_move_index = chess.parse_square(str(player_move)[-2:])
+
+            #Add the original square value based on the piece just moved. NOTE: before incentivizing sqaures occurs
+            if player_chess_piece == "p":
+                ORIGINAL_SQUARE_VALUE[player_move] = pawntable[player_move_index]
+            elif player_chess_piece == "r":
+                ORIGINAL_SQUARE_VALUE[player_move] = rookstable[player_move_index]
+            elif player_chess_piece == "n":
+                ORIGINAL_SQUARE_VALUE[player_move] = knightstable[player_move_index]
+            elif player_chess_piece == "b":
+                ORIGINAL_SQUARE_VALUE[player_move] = bishopstable[player_move_index]
+            elif player_chess_piece == "q":
+                ORIGINAL_SQUARE_VALUE[player_move] = queenstable[player_move_index]
+            else:
+                ORIGINAL_SQUARE_VALUE[player_move] = kingstable[player_move_index]
+
+            CURRENT_SQUARE_PIECE.append(player_chess_piece)
+
+            #Increases a square's value, based on what piece is there
+            #NOTE: YOU MUST UPDATE ALL TABLES
+            if player_chess_piece == "p":
+                pawntable[player_move_index] = pawntable[player_move_index] + CHESS_PIECE_VALUES[chess.PAWN]
+                rookstable[player_move_index] = rookstable[player_move_index] + CHESS_PIECE_VALUES[chess.PAWN]
+                knightstable[player_move_index] = knightstable[player_move_index] + CHESS_PIECE_VALUES[chess.PAWN]
+                bishopstable[player_move_index] = bishopstable[player_move_index] + CHESS_PIECE_VALUES[chess.PAWN]
+                queenstable[player_move_index] = queenstable[player_move_index] + CHESS_PIECE_VALUES[chess.PAWN]
+                kingstable[player_move_index] = kingstable[player_move_index] + CHESS_PIECE_VALUES[chess.PAWN]
+            elif player_chess_piece == "r":
+                pawntable[player_move_index] = pawntable[player_move_index] + CHESS_PIECE_VALUES[chess.ROOK]
+                rookstable[player_move_index] = rookstable[player_move_index] + CHESS_PIECE_VALUES[chess.ROOK]
+                knightstable[player_move_index] = knightstable[player_move_index] + CHESS_PIECE_VALUES[chess.ROOK]
+                bishopstable[player_move_index] = bishopstable[player_move_index] + CHESS_PIECE_VALUES[chess.ROOK]
+                queenstable[player_move_index] = queenstable[player_move_index] + CHESS_PIECE_VALUES[chess.ROOK]
+                kingstable[player_move_index] = kingstable[player_move_index] + CHESS_PIECE_VALUES[chess.ROOK]
+            elif player_chess_piece == "n":
+                pawntable[player_move_index] = pawntable[player_move_index] + CHESS_PIECE_VALUES[chess.KNIGHT]
+                rookstable[player_move_index] = rookstable[player_move_index] + CHESS_PIECE_VALUES[chess.KNIGHT]
+                knightstable[player_move_index] = knightstable[player_move_index] + CHESS_PIECE_VALUES[chess.KNIGHT]
+                bishopstable[player_move_index] = bishopstable[player_move_index] + CHESS_PIECE_VALUES[chess.KNIGHT]
+                queenstable[player_move_index] = queenstable[player_move_index] + CHESS_PIECE_VALUES[chess.KNIGHT]
+                kingstable[player_move_index] = kingstable[player_move_index] + CHESS_PIECE_VALUES[chess.KNIGHT]
+            elif player_chess_piece == "b":
+                pawntable[player_move_index] = pawntable[player_move_index] + CHESS_PIECE_VALUES[chess.BISHOP]
+                rookstable[player_move_index] = rookstable[player_move_index] + CHESS_PIECE_VALUES[chess.BISHOP]
+                knightstable[player_move_index] = knightstable[player_move_index] + CHESS_PIECE_VALUES[chess.BISHOP]
+                bishopstable[player_move_index] = bishopstable[player_move_index] + CHESS_PIECE_VALUES[chess.BISHOP]
+                queenstable[player_move_index] = queenstable[player_move_index] + CHESS_PIECE_VALUES[chess.BISHOP]
+                kingstable[player_move_index] = kingstable[player_move_index] + CHESS_PIECE_VALUES[chess.BISHOP]
+            elif player_chess_piece == "q":
+                pawntable[player_move_index] = pawntable[player_move_index] + CHESS_PIECE_VALUES[chess.QUEEN]
+                rookstable[player_move_index] = rookstable[player_move_index] + CHESS_PIECE_VALUES[chess.QUEEN]
+                knightstable[player_move_index] = knightstable[player_move_index] + CHESS_PIECE_VALUES[chess.QUEEN]
+                bishopstable[player_move_index] = bishopstable[player_move_index] + CHESS_PIECE_VALUES[chess.QUEEN]
+                queenstable[player_move_index] = queenstable[player_move_index] + CHESS_PIECE_VALUES[chess.QUEEN]
+                kingstable[player_move_index] = kingstable[player_move_index] + CHESS_PIECE_VALUES[chess.QUEEN]
+            else:
+                pawntable[player_move_index] = pawntable[player_move_index] + CHESS_PIECE_VALUES[chess.KING]
+                rookstable[player_move_index] = rookstable[player_move_index] + CHESS_PIECE_VALUES[chess.KING]
+                knightstable[player_move_index] = knightstable[player_move_index] + CHESS_PIECE_VALUES[chess.KING]
+                bishopstable[player_move_index] = bishopstable[player_move_index] + CHESS_PIECE_VALUES[chess.KING]
+                queenstable[player_move_index] = queenstable[player_move_index] + CHESS_PIECE_VALUES[chess.KING]
+                kingstable[player_move_index] = kingstable[player_move_index] + CHESS_PIECE_VALUES[chess.KING]
+
+
+            #Checks if a sqaure value has moved. IF the piece is no longer there, revert the square to its orginal value
+            for key, value in ORIGINAL_SQUARE_VALUE.items():
+                if BOARD.piece_at(chess.parse_square(key[-2:])) == None or str(BOARD.piece_at(chess.parse_square(key[-2:]))) != CURRENT_SQUARE_PIECE[index]:
+                    pawntable[chess.parse_square(str(key[-2:]))] = value
+                    rookstable[chess.parse_square(str(key[-2:]))] = value
+                    knightstable[chess.parse_square(str(key[-2:]))] = value
+                    bishopstable[chess.parse_square(str(key[-2:]))] = value
+                    queenstable[chess.parse_square(str(key[-2:]))] = value
+                    kingstable[chess.parse_square(str(key[-2:]))] = value
+                index += 1
+        else:
+            PLAYER_COLOR_WHITE = False
+
         
         #Find all legal moves and add the move name and sqaure index value into a dict
         for move in list(BOARD.legal_moves):
             #The key is the entire move; we use [2:] to get the destination square
             CHESS_SQUARE_INDEX[str(move)] = chess.parse_square(str(move)[-2:])
+
 
         #For every legal move, get the value of its destination square
         for key, value in CHESS_SQUARE_INDEX.items():
@@ -92,40 +262,27 @@ def evaluate_board():
             BOARD_REPLICA.push_san(key)
 
             #Look for the piece and using the index from the dict; use the table that matches the piece NOTE: List is reversed for black
-            #NOTE: P- Pawn, N- Knight, B- Bishop, Q- Queen, K- King  
-            if str(BOARD_REPLICA.piece_at(chess.parse_square(key[-2:]))) == "P":
-                if WHITE == False:
-                    CHESS_SQUARE_VALUE[key] = pawntable[value]
-                else:
-                    CHESS_SQUARE_VALUE[key] = pawntable[-value]
-            elif str(BOARD_REPLICA.piece_at(chess.parse_square(key[-2:]))) == "R":
-                if WHITE == False:
-                    CHESS_SQUARE_VALUE[key] = rookstable[value]
-                else:
-                    CHESS_SQUARE_VALUE[key] = rookstable[-value]
-            elif str(BOARD_REPLICA.piece_at(chess.parse_square(key[-2:]))) == "N":
-                if WHITE == False:
-                    CHESS_SQUARE_VALUE[key] = knightstable[value]
-                else:
-                    CHESS_SQUARE_VALUE[key] = knightstable[-value]
-            elif str(BOARD_REPLICA.piece_at(chess.parse_square(key[-2:]))) == "B":
-                if WHITE == False:
-                    CHESS_SQUARE_VALUE[key] = bishopstable[value]
-                else:
-                    CHESS_SQUARE_VALUE[key] = bishopstable[-value]
-            elif str(BOARD_REPLICA.piece_at(chess.parse_square(key[-2:]))) == "Q":
-                if WHITE == False:
-                    CHESS_SQUARE_VALUE[key] = queenstable[value]
-                else:
-                    CHESS_SQUARE_VALUE[key] = queenstable[-value]
+            #NOTE: P- Pawn, N- Knight, B- Bishop, Q- Queen, K- King
+
+            #Finds the chess piece on the inputed square
+            chess_piece = str(BOARD_REPLICA.piece_at(chess.parse_square(key[-2:])))
+
+            if chess_piece == "p" or chess_piece == "P":
+                CHESS_SQUARE_VALUE[key] = pawntable[value]
+            elif chess_piece == "r" or chess_piece == "R":
+                CHESS_SQUARE_VALUE[key] = rookstable[value]
+            elif chess_piece == "n" or chess_piece == "N":
+                CHESS_SQUARE_VALUE[key] = knightstable[value]
+            elif chess_piece == "b" or chess_piece == "B":
+                CHESS_SQUARE_VALUE[key] = bishopstable[value]
+            elif chess_piece == "q" or chess_piece == "Q":
+                CHESS_SQUARE_VALUE[key] = queenstable[value]
             else:
-                if WHITE == False:
-                    CHESS_SQUARE_VALUE[key] = kingstable[value]
-                else:
-                    CHESS_SQUARE_VALUE[key] = kingstable[-value]
+                CHESS_SQUARE_VALUE[key] = kingstable[value]
 
             #Reset after each use
             BOARD_REPLICA.pop()
+
         #Sort the list from greatest value, to least and select the highest value
         BOARD.push_san(sorted(CHESS_SQUARE_VALUE, key=CHESS_SQUARE_VALUE.get, reverse=True)[0])
 
@@ -134,13 +291,12 @@ def evaluate_board():
         print(f"Last move made was {str(BOARD.peek())}")
         print(BOARD)
         print(" ")
-        print(CHESS_SQUARE_VALUE)
-        print(CHESS_SQUARE_INDEX)
 
         #Clears index and values to re populate
         CHESS_SQUARE_INDEX.clear()
         CHESS_SQUARE_VALUE.clear()
-
+        index = 0
+        
 
 
         
